8a9fe34149eb137637ab9c0780a615bc37e88818
Brent Woodcock <brentwoodcock@gmail.com>
2017-09-08 18:32:58 -0500

    Added basic integration with Sitelink API using test location

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var sitelinkService = require('sitelinkservice/dist/SitelinkService');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -17,6 +18,24 @@ app.use(express.static(process.cwd() + '/dist'));
 
 app.get('/', function(req, res) {
 	res.send('./dist/index.html');
+});
+
+app.get('/api/unit', function(req, res) {
+	console.log(req);
+	console.log(res);
+	sitelinkService.getUnitInformation('l007').then(function (result) {
+		res.json(result.UnitsInformation_v2Result.diffgram.NewDataSet.Table);
+		console.log(result.UnitsInformation_v2Result.diffgram.NewDataSet.Table.length);
+	});
+});
+
+app.get('/api/type', function(req, res) {
+	console.log(req);
+	console.log(res);
+	sitelinkService.getTypeInformation('l007').then(function (result) {
+		res.json(result.UnitTypePriceList_v2Result.diffgram.NewDataSet.Table);
+		console.log(result.UnitTypePriceList_v2Result.diffgram.NewDataSet.Table.length);
+	});
 });
 
 app.listen(PORT, function() {
