14224288414f3a6238d43391f989f8645f993732
Brent Woodcock <brentwoodcock@gmail.com>
2017-09-08 17:24:05 -0500

    Initial React and Webpack configuration

---
+++ app/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LocationDropdown from './components/LocationDropdown';
+
+ReactDOM.render(<LocationDropdown />,document.getElementById('root'));
